e45f4f4e1fd9e0ff4779778eeb369b755c47eccd
0x6D-developerBYmik34 <63368593+0x6D-developerBYmik34@users.noreply.github.com>
2021-06-14 12:57:38 +0300

    some settings and config for deploy

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^ph@*8@22p*2j&itel0+bcyn8caqzlxm#loavxvmmr#lp^na9i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
